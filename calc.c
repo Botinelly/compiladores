6de7346dfc61ded44caf434246bd9f3899fb9b39
Botinelly <lucas_botinelly@hotmail.com>
2021-09-06 15:54:44 -0400

    Update calc.c

--- calc.c
+++ calc.c
@@ -1,39 +1,121 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
-int main(int argc, const char *argv[])
+char stack[256];
+int top = -1;
+
+void push(char i){
+    stack[++top] = i;
+}
+
+int symbol(char element)
 {
-        float num1=0, num2=0;
-        char oper;
-        num1 = atof(argv[1]);
-        num2 = atof(argv[3]);
-        oper = *argv[2];
+    switch(element){
+        case '#':
+            return 0;
+        case '(':
+            return 1;
+        case '+':
+        case '-':
+            return 2;
+        case '*':
+        case '/':
+            return 3;
+    }
+}
 
-        printf("%.2f %c %.2f = ", num1,oper,num2);
-
-        switch( oper )
+int calculator(char *postfix)
+{
+    char ch;
+    int i = 0;
+    int a, b;
+    while((ch = postfix[i++]) != 0)
+    {
+        if(isdigit(ch))
         {
-            case '+':
-                    printf("%.2f\n\n", num1 + num2);
+            push(ch-'0');
+        }
+        else{
+            a = stack[top--];
+            b = stack[top--];
+            switch(ch){
+                case '+':
+                    push(a + b);
                     break;
+                case '-':
+                    push(a - b);
+                    break;
+                case '*':
+                    push(a * b);
+                    break;
+                case '/':
+                    push(a / b);
+                    break;
+            }
+        }
+    }
+    return stack[top];
+}
 
-            case '-':
-                    printf("%.2f\n\n", num1 - num2);
-                    break;
+void to_RPN(char *infix, char *postfix)
+{
+    char ch, element;
+    int i = 0, k = 0;
 
-            case '*':
-                    printf("%.2f\n\n", num1 * num2);
-                    break;
+    push('#');
+    while((ch = infix[i++]) != '\0')
+    {
+        if(ch == '(')
+        {
+            push(ch);
+        }
+        else if (isalnum(ch))
+        {
+            postfix[k++] = ch;
+        }
+        else if (ch == ')')
+        {
+            while(stack[top] != '(')
+            {
+                postfix[k++] = stack[top--];
+            }
+            element = stack[top--];
+        } else 
+        {
+            while(symbol(stack[top]) >= symbol(ch))
+            {
+                postfix[k++] = stack[top--];
+            }
+            push(ch);
+        }
+    }
+    while (stack[top] != '#')
+    {
+        postfix[k++] = stack[top--];
+    }
+    postfix[k] = 0;
+}
 
-            case '/':
-                    if(num2 != 0)
-                        printf("%.2f\n\n", num1 / num2);
-                    else
-                        printf("There is no division by 0\n\n");
-                    break;
 
-            case '%':
-                    printf("%d\n\n", (int)num1 % (int)num2);
-                    break;
+int main(int argc, char** argv)
+{
+    int a, b;
+    char input[80], postf[80];
+
+    if(argc == 1)
+    {
+        while(fgets(input, sizeof(input), stdin) != NULL)
+        {
+            input[strlen(input) - 1] = 0;
+            to_RPN(input, postf);
+            printf("%s=%d\n", input, calculator(postf));
+        }
+    }
+    else
+    {
+        strcpy(input, argv[1]);
+        to_RPN(input, postf);
+        printf("%s=%d\n", input, calculator(postf));
     }
 }
